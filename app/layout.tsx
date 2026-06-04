bd95c666a0c11e652c7a195a3eb0883358dcedf1
standshorts-oss <standshorts@gmail.com>
2026-06-04 15:26:08 +0000

    fix design and tailwind config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,19 @@
+import './globals.css' // <-- ЭТО ОБЯЗАТЕЛЬНО
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Gigachad Casino',
+  description: 'The most honest casino ever',
+}
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
